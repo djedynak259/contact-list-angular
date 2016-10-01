8f822412ecd20dec964fca570c1120aae5aacba0
Dan J <danj@stitchlabs.com>
2016-10-01 16:21:29 -0700

    updated things

--- js/main-new.js
+++ js/main-new.js
@@ -25,21 +25,6 @@ angular.module("ContactListApp", [])
 	}
 })
 
-.factory('ContactsDataset', function() {
-	return [
-	{
-		firstName:"dan",
-		lastName:"theman",
-		cellNum:"(123)-123-1234"
-	}, 
-	{
-		firstName:"ben",
-		lastName:"randy",
-		cellNum:"(123)-223-2234"
-	}
-	];
-})
-
 .controller('ListCtrl', function($scope, ContactsDataset) {
 	$scope.contacts = ContactsDataset;
 })
@@ -56,6 +41,7 @@ angular.module("ContactListApp", [])
 
 	$scope.saveContact = function() {
 		ContactsUtil.addContact($scope.newcont);
+		$scope.newcont = {};
 	}
 })
 
@@ -68,6 +54,7 @@ angular.module("ContactListApp", [])
 
 	return {
 		restrict: 'E',
+		// templateUrl: 'contact-card.html';
 		template: "<div class='col-md-2 contactCard' ng-repeat='contact in dataset | filter:query.searchQuery'><h4>{{contact.firstName}}</h4><h4>{{contact.lastName}}</h4><h4>{{contact.cellNum}}</h4></div>",
 		scope: {
 			dataset: '='
@@ -75,4 +62,66 @@ angular.module("ContactListApp", [])
 		controller: controller
 	
 	};
-});
+})
+
+.factory('ContactsDataset', function() {
+	return [
+		{
+			firstName:"dan",
+			lastName:"theman",
+			cellNum:"(123)-123-1234"
+		}, 
+		{
+			firstName:"ben",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"sara",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"sam",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"zach",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"pat",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"ben",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"phil",
+			lastName:"johnson",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"ben",
+			lastName:"johnson",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"ben",
+			lastName:"johnson",
+			cellNum:"(123)-223-2234"
+		},
+		{
+			firstName:"chris",
+			lastName:"randy",
+			cellNum:"(123)-223-2234"
+		}					
+	];
+})
+
+;
